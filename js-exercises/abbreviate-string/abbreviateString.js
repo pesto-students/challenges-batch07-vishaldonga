96ed81133789e9e86d7eb9ab87c7701e3266b77c
Vishal Donga <vishaldonga811@gmail.com>
2021-02-11 12:52:04 +0530

    Add try-catch block

--- js-exercises/abbreviate-string/abbreviateString.js
+++ js-exercises/abbreviate-string/abbreviateString.js
@@ -1,16 +1,20 @@
 function abbreviateString(str) {
   //  Checking for invalid inputs
-  if (typeof str !== 'string') {
-    throw new Error('Invalid Parameters');
+  try {
+    if (!str && typeof str !== 'string') {
+      throw new Error('Invalid Parameters');
+    }
+    const arrayWords = str.split(' ');
+    const arraySize = arrayWords.length;
+    //  If given input string is single word then it will return
+    //  word itself else it will return abbreviated string
+    if (arraySize === 1) {
+      return arrayWords[0];
+    }
+    return `${arrayWords[0]} ${arrayWords[arraySize - 1][0].toUpperCase()}.`;
+  } catch (e) {
+    throw e.message;
   }
-  const arrayWords = str.split(' ');
-  const arraySize = arrayWords.length;
-  //  If given input string is single word then it will return
-  //  word itself else it will return abbreviated string
-  if (arraySize === 1) {
-    return arrayWords[0];
-  }
-  return `${arrayWords[0]} ${arrayWords[arraySize - 1][0].toUpperCase()}.`;
 }
 
 export { abbreviateString };
--- js-exercises/duplicate-letters/duplicateLetters.js
+++ js-exercises/duplicate-letters/duplicateLetters.js
@@ -1,25 +1,32 @@
 function duplicateLetters(...args) {
-  //  Converting given string to sorted array
-  const mainArray = [...args][0].split('').sort();
-  let count = 1;
-  let maxCount = 1;
-  const tempArray = [];
-  //  Checking every character and its count
-  for (let i = 0; i < mainArray.length; i += 1) {
-    if (tempArray.indexOf(mainArray[i]) < 0) {
-      tempArray.push(mainArray[i]);
-      count = 1;
-    } else {
-      count += 1;
-      if (maxCount < count) {
-        maxCount = count;
+  try {
+    if (args) {
+      //  Converting given string to sorted array
+      const mainArray = [...args][0].split('').sort();
+      let count = 1;
+      let maxCount = 1;
+      const tempArray = [];
+      //  Checking every character and its count
+      for (let i = 0; i < mainArray.length; i += 1) {
+        if (tempArray.indexOf(mainArray[i]) < 0) {
+          tempArray.push(mainArray[i]);
+          count = 1;
+        } else {
+          count += 1;
+          if (maxCount < count) {
+            maxCount = count;
+          }
+        }
       }
+      if (maxCount === 1) {
+        return false;
+      }
+      return maxCount;
     }
+    throw new Error('No arguments passed');
+  } catch (e) {
+    throw e.message;
   }
-  if (maxCount === 1) {
-    return false;
-  }
-  return maxCount;
 }
 
 export {
