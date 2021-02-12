9b8a681563f4b1de7f29df4bab3f1ee2f7191243
Vishal Donga <vishaldonga811@gmail.com>
2021-02-12 13:29:55 +0530

    exercise-solutions/day-03

---
+++ js-exercises/rot-13/rot13.js
@@ -0,0 +1,21 @@
+function rot13(str) {
+  if (!str || typeof str !== 'string') return false;
+  const arrChar = [];
+  for (const char of str) {
+    if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) {
+      if (char.charCodeAt(0) <= 77) {
+        arrChar.push(String.fromCharCode(char.charCodeAt(0) + 13));
+      } else {
+        arrChar.push(String.fromCharCode(char.charCodeAt(0) - 26 + 13));
+      }
+    } else {
+      arrChar.push(char);
+    }
+  }
+  const decodedString = arrChar.join('');
+  return decodedString;
+}
+
+export {
+  rot13,
+};
---
+++ js-exercises/sum-fibs-memoized/sumFibsMemoized.js
@@ -0,0 +1,31 @@
+function sumFibs(num) {
+  if (!num || typeof num !== 'number' || num <= 0) return false;
+  if (num === 1 || num === 2) return num;
+  let i = 1;
+  let j = 1;
+  let nextNumber = 0;
+  let sum = 0;
+  do {
+    nextNumber = i + j;
+    i = j;
+    j = nextNumber;
+    if (nextNumber % 2 !== 0) {
+      sum += nextNumber;
+    }
+  } while (num > (i + j));
+  return (sum + 2);
+}
+
+function cacheFunction() {
+  const cache = {};
+  return (n) => {
+    if (n in cache) {
+      return cache[n];
+    }
+    const storeValue = sumFibs(n);
+    cache[n] = storeValue;
+    return storeValue;
+  };
+}
+
+export { sumFibs, cacheFunction };
