6d3ec2de2955a8baf7ebc53da6f9c03a8ebe65f5
Vishal Donga <vishaldonga811@gmail.com>
2021-02-17 11:57:57 +0530

    update sunEvenArgs with new language features

--- js-exercises/sum-even-args/sumEvenArgs.js
+++ js-exercises/sum-even-args/sumEvenArgs.js
@@ -1,17 +1,7 @@
-/* eslint-disable prefer-arrow-callback */
 const sumEvenArgs = (...args) => {
   if (!args) return 0;
-  const evenArgs = args.filter(
-    function checkForEven(num) {
-      return (num % 2 === 0);
-    },
-  );
-  const result = evenArgs.reduce(
-    function callBack(totalSum, number) {
-      return (totalSum + number);
-    }, 0,
-  );
-  return result;
+  const evenArgs = args.filter((num) => (num % 2 === 0));
+  return evenArgs.reduce((totalSum, number) => (totalSum + number), 0);
 };
 
 export { sumEvenArgs };
